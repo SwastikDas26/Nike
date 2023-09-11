c4d9056d83170fb256d2b2b7059874ed125c2711
Swastik <sr6015@srmist.edu.in>
2023-09-11 22:54:40 +0530

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+
+export {
+    Hero, 
+    PopularProducts, 
+    SuperQuality, 
+    Services, 
+    SpecialOffers,
+    CustomerReviews, 
+    Subscribe,
+    Footer
+}
